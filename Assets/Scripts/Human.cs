976563b398c750a014dbfa60cf7938429b4c746b
Priyank Jain <fly2priyank@gmail.com>
2020-03-15 17:29:20 -0700

    - Introducing Human and PlayerController inherits from Human

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    private void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+
+        m_state = HealthState.cHealthy;
+        Health = 100;
+
+        m_symptoms = null; // no symptoms exist on a new healthy human
+    }
+
+    enum HealthState
+    {
+        cHealthy,
+        cInfected,
+        cRecovered // human recovered after being infected 
+    };
+    private HealthState m_state;
+
+    enum Symptoms
+    {
+        cNone, // human is healthy
+        cCough,
+        cSneeze,
+        cBreathless,
+        cFever
+    }
+    private Symptoms[] m_symptoms;
+
+    private int m_health;
+    public int Health
+    {
+        get { return m_health; }
+        set
+        {
+            if (m_health != value)
+            {
+                m_health = Mathf.Clamp(value, 0, 100);
+
+                Gradient gradient = new Gradient();
+                GradientColorKey[] colorKeys = new GradientColorKey[2];
+                colorKeys[0].color = Color.red;
+                colorKeys[0].time = 0.0f;
+                colorKeys[1].color = Color.green;
+                colorKeys[1].time = 1.0f;
+
+                GradientAlphaKey[] alphaKeys = new GradientAlphaKey[2];
+                alphaKeys[0].alpha = 1.0f;
+                alphaKeys[0].time = 0.0f;
+                alphaKeys[1].alpha = 1.0f;
+                alphaKeys[1].time = 1.0f;
+
+                gradient.SetKeys(colorKeys, alphaKeys);
+
+                m_spriteRenderer.color = gradient.Evaluate(m_health / 100f);
+            }
+        }
+    }
+
+    private SpriteRenderer m_spriteRenderer;
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,45 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Human
 {
     public float speed;             //Floating point variable to store the player's movement speed.
     public Text countText;          //Store a reference to the UI Text component which will display the number of pickups collected.
     public Text winText;            //Store a reference to the UI Text component which will display the 'You win' message.
 
     private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-    private SpriteRenderer m_spriteRenderer;
 
     private int count;              //Integer to store the number of pickups collected so far.
-
-    private int health;
-    public int Health {
-        get { return health; }
-        set 
-        {
-            if (health != value)
-            {
-                health = Mathf.Clamp(value, 0, 100);
-
-                Gradient gradient = new Gradient();
-                GradientColorKey[] colorKeys = new GradientColorKey[2];
-                colorKeys[0].color = Color.red;
-                colorKeys[0].time = 0.0f;
-                colorKeys[1].color = Color.green;
-                colorKeys[1].time = 1.0f;
-
-                GradientAlphaKey[] alphaKeys = new GradientAlphaKey[2];
-                alphaKeys[0].alpha = 1.0f;
-                alphaKeys[0].time = 0.0f;
-                alphaKeys[1].alpha = 1.0f;
-                alphaKeys[1].time = 1.0f;
-
-                gradient.SetKeys(colorKeys, alphaKeys);
-
-                m_spriteRenderer.color = gradient.Evaluate(health/100f);
-            }
-        } 
-    }
 
     private Touch theTouch;
     private Vector2 touchStartPosition, touchEndPosition;
@@ -51,10 +21,6 @@ public class PlayerController : MonoBehaviour
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D>();
-
-        m_spriteRenderer = GetComponent<SpriteRenderer>();
-
-        Health = 100;
 
         //Initialize count to zero.
         count = 0;
