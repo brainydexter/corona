28eab96ed6a98a0acf053b6cafdd7c03fa7d9e11
Priyank Jain <fly2priyank@gmail.com>
2020-03-24 12:10:04 -0700

    - randomly generating N humans

--- Assets/Scripts/HumanManager.cs
+++ Assets/Scripts/HumanManager.cs
@@ -10,26 +10,37 @@ public class HumanManager : MonoBehaviour
         Debug.Assert(m_pHuman != null, "Human prefab not assigned");
 
         //int N = Random.Range(2, 5);
-        int N = 2;
+        int N = 33;
+
+        int dia = 4;
+        int extents = 24;
+        int dims = extents / dia;
+
+        HashSet<int> set = new HashSet<int>();
+
+        while(set.Count != N)
+        {
+            set.Add(UnityEngine.Random.Range(0, dims * dims));
+        }
 
         m_humans = new Human[N];
 
-        for(int i = 0; i < N; ++i)
+        int i = 0;
+        foreach(var index in set)
         {
+            int row = index / dims; row -= (dims/2); row *= (dia/1);
+            int col = index % dims; col -= (dims/2); col *= (dia/1);
+
             m_humans[i] = Instantiate<Human>(m_pHuman, gameObject.transform);
+            m_humans[i].transform.position = new Vector3(col, row, 0);
 
-            if(i % 2 == 0)
-            {
-                m_humans[i].transform.position = new Vector3(0, UnityEngine.Random.Range(1, 10.5f), 0);
-            }
-            else
-            {
-                m_humans[i].transform.position = new Vector3(UnityEngine.Random.Range(1, 10.5f), 0, 0);
-            }
+            ++i;
         }
+    }
 
-        int randomIndex = UnityEngine.Random.Range(0, N);
-        m_humans[randomIndex].Infect(Symptoms.cCough);
+    private void Start()
+    {
+        m_humans[0].m_rigidBody.AddForce(UnityEngine.Random.insideUnitCircle * 500f);
     }
 
     // Update is called once per frame
