5c3672c7006c7a54c4aca0378a77a23242429958
Priyank Jain <fly2priyank@gmail.com>
2020-03-31 14:35:45 -0700

    blocks follows player mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Text countText;          //Store a reference to the UI Text component which will display the number of pickups collected.
     public Text winText;            //Store a reference to the UI Text component which will display the 'You win' message.
@@ -14,15 +16,19 @@ public class PlayerController : MonoBehaviour
     private Vector2 touchStartPosition, touchEndPosition;
 
     private Rigidbody2D m_rigidBody;
+    private Collider2D m_collider;
 
-    private Vector3 mousePosition;
+    private Vector2 m_lastPosition;
     private Vector2 direction;
     private float moveSpeed = 100f;
+
+    private bool m_isDragging = false;
 
     // Use this for initialization
     void Awake()
     {
         m_rigidBody = GetComponent<Rigidbody2D>();
+        m_collider = GetComponent<Collider2D>();
 
         //Initialize count to zero.
         count = 0;
@@ -37,40 +43,40 @@ public class PlayerController : MonoBehaviour
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
-        {
-            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            direction = (mousePosition - transform.position).normalized;
-            m_rigidBody.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
-        }
-        else
-        {
-            m_rigidBody.velocity = Vector2.zero;
-        }
+        //if (Input.GetMouseButton(0))
+        //{
+        //    m_lastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //    direction = (m_lastPosition - transform.position).normalized;
+        //    m_rigidBody.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+        //}
+        //else
+        //{
+        //    m_rigidBody.velocity = Vector2.zero;
+        //}
     }
 
     private void Update()
     {
         if (Input.touchCount > 0)
         {
-            theTouch = Input.GetTouch(0);
+            //theTouch = Input.GetTouch(0);
 
-            if (theTouch.phase == TouchPhase.Began)
-            {
-                touchStartPosition = theTouch.position;
-            }
+            //if (theTouch.phase == TouchPhase.Began)
+            //{
+            //    touchStartPosition = theTouch.position;
+            //}
 
-            else if (theTouch.phase == TouchPhase.Moved || theTouch.phase == TouchPhase.Ended)
-            {
-                touchEndPosition = theTouch.position;
+            //else if (theTouch.phase == TouchPhase.Moved || theTouch.phase == TouchPhase.Ended)
+            //{
+            //    touchEndPosition = theTouch.position;
 
-                float x = touchEndPosition.x - touchStartPosition.x;
-                float y = touchEndPosition.y - touchStartPosition.y;
+            //    float x = touchEndPosition.x - touchStartPosition.x;
+            //    float y = touchEndPosition.y - touchStartPosition.y;
 
-                Vector2 force;
+            //    Vector2 force;
 
                
-            }
+            //}
         }
     }
 
@@ -85,4 +91,48 @@ public class PlayerController : MonoBehaviour
             //... then set the text property of our winText object to "You win!"
             winText.text = "You win!";
     }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        m_isDragging = true;
+        m_collider.enabled = false;
+        m_lastPosition = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 displacement = eventData.position - m_lastPosition;
+        //displacement.Normalize();
+
+        var worldPosition = Camera.main.ScreenToWorldPoint(eventData.position);
+
+        m_rigidBody.MovePosition(worldPosition);
+
+        //Debug.Log(worldPosition + " :: " + transform.position);
+        Debug.Log("drag-ing");
+
+        //var positionDifference = worldPosition - transform.position;
+
+        //if(positionDifference.magnitude < threshold)
+        //{
+        //    m_rigidBody.velocity = Vector2.zero;
+        //}
+        //else
+        //{
+        //    m_rigidBody.AddForce(displacement * 1f);
+        //}
+
+        m_lastPosition = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("drag end");
+        m_isDragging = false;
+        m_rigidBody.velocity = Vector2.zero;
+
+        m_collider.enabled = true;
+    }
+
+    public float threshold = 1f;
 }
