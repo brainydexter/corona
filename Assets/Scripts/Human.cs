36a4f9a2acc569ececea0a675e4ff5f303ae9ecc
Priyank Jain <fly2priyank@gmail.com>
2020-04-01 17:59:09 -0700

    removing enum symptoms from human

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -19,13 +19,10 @@ public class Human : MonoBehaviour
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         m_rigidBody = GetComponent<Rigidbody2D>();
 
-        m_healthStateMachine = new HealthStateMachine();
+        m_healthStateMachine = new HealthStateMachine(this);
         //m_healthStateMachine.initialize(new HealthyState());
 
-        m_state = HealthState.cHealthy;
         Health = 100;
-
-        m_symptoms = null; // no symptoms exist on a new healthy human
     }
 
     private void Update()
@@ -38,73 +35,13 @@ public class Human : MonoBehaviour
         m_healthStateMachine.InfectWithCough();
     }
 
-    /// <summary>
-    /// Infect iff human is healthy. Infected humans are not affected when they come in contact with another infected human
-    /// Healthy -> Cough | Sneeze -> Breathlessnes -> Die | Recover
-    /// </summary>
-    /// <param name="symptom"></param>
-    private void Infect(Symptoms symptom)
-    {
-        Debug.Assert(symptom == Symptoms.cNone, "cannot be infected with no symptoms");
-
-        // if a human has recovered, lets assume he can't be infected again
-        if (m_state == HealthState.cRecovered)
-            return;
-
-        if (m_symptoms == null)
-        {
-            m_symptoms = new Symptoms[4];
-        }
-
-        // find if human is already infected with the same symptom
-        bool symptomExists = Array.Find(m_symptoms, s => s == symptom) == symptom;
-
-        Debug.Log("to infect: " + (symptomExists == false));
-
-        // try to infect 
-        for (int i = 0; !symptomExists && (i < m_symptoms.Length); ++i)
-        {
-            if(m_symptoms[i] == Symptoms.cNone)
-            {
-                m_symptoms[i] = symptom;
-                break;
-            }
-        }
-
-        m_state = HealthState.cInfected;
-
-        Health = 50;
-    }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         Human otherHuman = other.gameObject.GetComponent<Human>();
 
         if(otherHuman != null)
         {
-            Infect(otherHuman.HealthState1);
-
-            /*
-             * 1 - Healthy, 0 - Infected
-             * 
-             * M    1    0    1    0
-             * O    1    0    0    1
-             *      N    N    M0   Oo
-             */
-
-            if( (
-                (m_state == HealthState.cHealthy) ||
-                (m_state == HealthState.cInfected))
-                &&
-                (m_state == otherHuman.m_state))
-            {
-                return;
-            }
-
-            if(m_state == HealthState.cHealthy)
-            {
-                Infect(otherHuman.m_symptoms[0]);
-            }
+            Infect(otherHuman.HealthState);
 
             Debug.Log("I: " + name + " Collided with other human: " + otherHuman.name);
 
@@ -117,20 +54,7 @@ public class Human : MonoBehaviour
     }
 
     HealthStateMachine m_healthStateMachine;
-    public HealthStateMachine HealthState1 { get { return m_healthStateMachine; } }
-
-    enum HealthState
-    {
-        cHealthy, // never been infected before
-        cInfected,
-        cRecovered // human recovered after being infected 
-    };
-
-    [SerializeField]
-    private HealthState m_state;
-
-    [SerializeField]
-    private Symptoms[] m_symptoms;
+    public HealthStateMachine HealthState { get { return m_healthStateMachine; } }
 
     [SerializeField]
     private int m_health;
@@ -142,6 +66,14 @@ public class Human : MonoBehaviour
             if (m_health != value)
             {
                 m_health = Mathf.Clamp(value, 0, 100);
+
+                if(m_health == 30) // hack for now
+                {
+                    // recovered
+                    m_spriteRenderer.color = Color.white;
+                    return;
+                }
+
 
                 Gradient gradient = new Gradient();
                 GradientColorKey[] colorKeys = new GradientColorKey[2];
